1759d1d3e95830d48d3e1d9b854af02e1583e8da
Collins-Favour <frenchkaptain@gmail.com>
2026-09-10 12:03:16 +0300

    lesson 2

---
+++ values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(" Collins " + "Mwangi")
+	fmt.Println("7+23 =", 7+23)
+	fmt.Println("56.0/5.0 =", 56.0/5.0)
+	fmt.Println(45.3 * 8.9)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!!!true)
+}
---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Wonder"
+	fmt.Println(a)
+	var b, c, d = "woman", "is now available", " in German cinemas"
+	fmt.Println(a, b+"", c+" ", d)
+	var x, y int = 21, 789
+	fmt.Println(x, y)
+	f := 28
+	fmt.Println("in", f, "Cinemas ")
+}
