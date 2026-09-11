29fba44b9c0fca6d3acfc6108aec013365a3207d
Collins-Favour <frenchkaptain@gmail.com>
2026-09-11 10:36:36 +0300

    lesson 3

--- constants.go
+++ constants.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+const s = "Wonder"
+
 func main() {
+	fmt.Println(s)
+	const n = 22 / 7
+	const b = 50000
+	const d = 3e20 / b
+	fmt.Println(d)
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
 
 }
