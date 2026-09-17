4709d155429e68ac4a1e1b294f0f97c332603be5
Collins-Favour <frenchkaptain@gmail.com>
2026-09-17 15:56:03 +0300

    leson 7

--- Tafa.com/2_test.go
+++ Tafa.com/2_test.go
@@ -8,13 +8,19 @@ import (
 func TestCreateUserData2(t *testing.T) {
 	result := createUserData("paul", "07656500000655", 333)
 	if result.userName != "aul" {
-		t.Errorf("expected name 'aul' but got %s", result.userName)
+		t.Errorf("expected name 'aul' but got %s\n", result.userName)
 	}
-	if result.phoneNumber == "07656500000655" {
-		fmt.Printf("Correct number '%s' registered", result.phoneNumber)
+	if result.phoneNumber == "0765650000655" {
+		fmt.Printf("Correct number '%s' registered\n", result.phoneNumber)
 
 	} else {
-		fmt.Print("Eiii we need program needs work")
+		fmt.Println("Eiii the program needs work")
+	}
+	if result.userTickets == 333 {
+		fmt.Println("We are cruising Nicely")
+
+	} else {
+		fmt.Printf("Eii there's a problem \n We expected 333 we got	' %v '	", result.userTickets)
 	}
 
 }
