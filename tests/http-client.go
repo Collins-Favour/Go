761b27d26f11fc0bd9033442bf6c7adeb852cbb4
Collins-Favour <frenchkaptain@gmail.com>
2026-09-21 11:39:13 +0300

    12

--- tests/http-client.go
+++ tests/http-client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 )
@@ -12,4 +13,10 @@ func main() {
 	}
 	defer resp.Body.Close()
 	fmt.Println("Response Status:", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 500; i++ {
+		fmt.Println(scanner.Text())
+
+	}
 }
