a75a288b59f4a2c34d50532f49803b8b7fb98f67
Collins-Favour <frenchkaptain@gmail.com>
2026-09-22 15:21:24 +0300

    er

---
+++ TDD/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Haaland" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+}
+
+}
---
+++ TDD/server_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetPlayers(t *testing.T) {
+	t.Run("returns Halaand's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Halaand", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want % q", got, want)
+		}
+		t.Run("returns Floyd's score", func(t *testing.T) {
+			request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+			response := httptest.NewRecorder()
+
+			PlayerServer(response, request)
+
+			got := response.Body.String()
+			want := "10"
+
+			if got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		})
+	})
+}
